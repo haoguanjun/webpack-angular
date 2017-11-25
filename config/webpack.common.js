41ecb7a84c7fd5d99e6489ae2c1cab6e69a891eb
haoguanjun <haoguanjun@gmail.com>
2017-11-26 07:12:24 +0800

    upgrade webpack to 3.8.1.
    note:
    we can't use extract-text-webpack-pluginv3.0.2, it doesn't work.

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -3,64 +3,74 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var helpers = require('./helpers');
 
+/*
+ * 三个入口用于分别打包
+ * 
+ * extract-text-webpack-plugin 为了抽离 css 样式, 防止将样式打包在 js 中引起页面样式加载错乱的现象
+ * fallbackLoader 编译后用什么loader来提取css文件
+ */
 module.exports = {
-  entry: {
-    'polyfills': './src/polyfills.ts',
-    'vendor': './src/vendor.ts',
-    'app': './src/main.ts'
-  },
+	entry: {
+		'polyfills': './src/polyfills.ts',
+		'vendor': './src/vendor.ts',
+		'app': './src/main.ts'
+	},
 
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
+	resolve: {
+		extensions: ['.ts', '.js']
+	},
 
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        loaders: [
-          {
-            loader: 'awesome-typescript-loader',
-            options: { configFileName: helpers.root('src', 'tsconfig.json') }
-          } , 'angular2-template-loader'
-        ]
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader'
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-        loader: 'file-loader?name=assets/[name].[hash].[ext]'
-      },
-      {
-        test: /\.css$/,
-        exclude: helpers.root('src', 'app'),
-        loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader?sourceMap' })
-      },
-      {
-        test: /\.css$/,
-        include: helpers.root('src', 'app'),
-        loader: 'raw-loader'
-      }
-    ]
-  },
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				loaders: [
+					{
+						loader: 'awesome-typescript-loader',
+						options: { configFileName: helpers.root('src', 'tsconfig.json') }
+					}, 
+					'angular2-template-loader'
+				]
+			},
+			{
+				test: /\.html$/,
+				loader: 'html-loader'
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+				loader: 'file-loader?name=assets/[name].[hash].[ext]'
+			},
+			{
+				test: /\.css$/,
+				exclude: helpers.root('src', 'app'),
+				loader: ExtractTextPlugin.extract({
+					fallbackLoader: 'style-loader', 
+					loader: 'css-loader?sourceMap' 
+				})
+			},
+			{
+				test: /\.css$/,
+				include: helpers.root('src', 'app'),
+				loader: 'raw-loader'
+			}
+		]
+	},
 
-  plugins: [
-    // Workaround for angular/angular#11580
-    new webpack.ContextReplacementPlugin(
-      // The (\\|\/) piece accounts for path separators in *nix and Windows
-      /angular(\\|\/)core(\\|\/)@angular/,
-      helpers.root('./src'), // location of your src
-      {} // a map of your routes
-    ),
+	plugins: [
+		// Workaround for angular/angular#11580
+		new webpack.ContextReplacementPlugin(
+			// The (\\|\/) piece accounts for path separators in *nix and Windows
+			/angular(\\|\/)core(\\|\/)@angular/,
+			helpers.root('./src'), // location of your src
+			{} // a map of your routes
+		),
 
-    new webpack.optimize.CommonsChunkPlugin({
-      name: ['app', 'vendor', 'polyfills']
-    }),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: ['app', 'vendor', 'polyfills']
+		}),
 
-    new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    })
-  ]
+		new HtmlWebpackPlugin({
+			template: 'src/index.html'
+		})
+	]
 };
